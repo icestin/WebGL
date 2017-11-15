20156f22c3ff8c90b9af6682d4f315c881c72976
icestin <ning231@126.com>
2017-11-15 15:47:22 +0800

    清空背景

--- HelloCanvas/HelloCanvas.js
+++ HelloCanvas/HelloCanvas.js
@@ -5,6 +5,8 @@ function main(){
         alert('渲染WebGL上下文失败');
         return;
     }
+    //指定清空<canvas>的颜色
     gl.clearColor(0.0,0.0,0.0,1.0);
-    
+    //清空<canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
