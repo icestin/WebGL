38c0e149631e59b8500ca14401e49243ac17cd9a
icestin <ning231@126.com>
2017-12-01 17:25:26 +0800

    添加旋转矩阵

---
+++ chapter4/RotatedTriagnle_Matrix/index.js
@@ -0,0 +1,88 @@
+
+//顶点着色器程序
+var VSHADER_SOURCE=
+'attribute vec4 a_Position;\n'+
+'uniform mat4 u_xformMatrix;\n'+
+'void main() {\n' +
+' gl_Position = u_xformMatrix * a_Position;\n'+ //设置坐标
+'}\n';
+//片源着色器程序
+var FSHADER_SOURCE=
+'void main() {\n'+
+' gl_FragColor=vec4(1.0,0.0,0.0,1.0);\n'+ //设置颜色
+'}\n';
+var ANGLE = 2*60.0;
+function main(){
+    var canvas=document.getElementById('webgl');
+    var gl=getWebGLContext(canvas);
+    if(!gl){
+        alert('渲染WebGL上下文失败');
+        return;
+    }
+    //初始化着色器
+    if(!initShaders(gl,VSHADER_SOURCE,FSHADER_SOURCE)){
+        console.log('初始化着色器失败');
+        return;
+    }
+    //设置顶点位置
+    var n = initVertexBuffers(gl);
+    if(n < 0) {
+        console.log('设置顶点位置失败');
+        return;
+    }
+    //将旋转 指定点顶点着色器
+    var radian = Math.PI * ANGLE /180.0;  //转为弧度制
+    var cosB=Math.cos(radian),sinB=Math.sin(radian);
+    // 为旋转矩阵创建 Matrix4
+    var xformMatrix = new Matrix4();
+    // 为xformMatrix 设置为旋转矩阵
+    xformMatrix.setRotate(ANGLE, 0, 0, 1);
+
+    var u_xformMatrix = gl.getUniformLocation(gl.program,'u_xformMatrix');
+    if(!u_xformMatrix){
+        console.log('获取角度信息设置失败');
+        return ;
+    }
+    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix.elements);
+      //指定清空<canvas>的颜色 即设置<canvas>背景色
+    gl.clearColor(0.0,0.0,0.0,1.0);//黑色     
+    
+    //清空<canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);//其实是清空颜色缓存区 ，除了颜色缓冲区，还有深度缓冲区及模板缓冲区
+
+    //绘制一个点
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+
+
+function initVertexBuffers(gl){
+    var vertices=new Float32Array([
+        0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+    ]);
+    var n=3; //点的个数
+    //创建缓充区对象
+    var vertexBuffer=gl.createBuffer();
+    if (!vertexBuffer) {
+      console.log('创建缓冲区对象失败');
+      reuturn -1;
+    }
+    //将缓冲区对象绑定到目标
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    //向缓充区对象写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    // 获取attribute变量的存储位置
+    var a_Position =gl.getAttribLocation(gl.program,'a_Position');
+    if(a_Position<0){
+        conosole.log('尝试获取a_Position失败');
+        return;
+    }
+    //将缓冲区对象分配给a_position对象
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    //连接a_Position变量与分配给它的缓冲区对象
+    gl.enableVertexAttribArray(a_Position);
+    return n;
+}
+
+
+ 
