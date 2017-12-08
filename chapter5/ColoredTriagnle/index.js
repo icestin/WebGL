9c840181e1946562d68a5d77d3fbf94a8f9311c4
icestin <ning231@126.com>
2017-12-08 12:55:35 +0800

    通过坐标控制片元颜色

---
+++ chapter5/ColoredTriagnle/index.js
@@ -0,0 +1,93 @@
+// 根据片元的位置来确定片元的颜色
+//可以证明片元着色器对每个片元都执行了一次
+//顶点着色器程序
+var VSHADER_SOURCE=
+'attribute vec4 a_Position;\n'+
+'void main() {\n' +
+' gl_Position = a_Position;\n'+ //设置坐标
+'}\n';
+//片源着色器程序
+/**
+ * 片元都带有坐标信息，vec4 gl_FragCoord 内置变量的第1个和第2个分量表示片元在
+ * <canvas>坐标系统中的坐标值。
+ */
+var FSHADER_SOURCE=
+'precision mediump float;\n'+
+'uniform float u_Width;\n'+
+'uniform float u_Height;\n'+
+'void main() {\n'+
+' gl_FragColor=vec4(gl_FragCoord.x/u_Width, 0.0, gl_FragCoord.y/u_Height, 1.0);\n'+ //设置颜色
+'}\n';
+
+function main(){
+    var canvas=document.getElementById('webgl');
+    var gl=getWebGLContext(canvas);
+    if(!gl){
+        alert('渲染WebGL上下文失败');
+        return;
+    }
+    //初始化着色器
+    if(!initShaders(gl,VSHADER_SOURCE,FSHADER_SOURCE)){
+        console.log('初始化着色器失败');
+        return;
+    }
+    //设置顶点位置
+    var n = initVertexBuffers(gl);
+    if(n < 0) {
+        console.log('设置顶点位置失败');
+        return;
+    }
+      //指定清空<canvas>的颜色 即设置<canvas>背景色
+    gl.clearColor(0.0,0.0,0.0,1.0);//黑色     
+    
+    //清空<canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);//其实是清空颜色缓存区 ，除了颜色缓冲区，还有深度缓冲区及模板缓冲区
+
+    //绘制一个点
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+
+
+function initVertexBuffers(gl){
+    var vertices=new Float32Array([
+        0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+    ]);
+    var n=3; //点的个数
+    //创建缓充区对象
+    var vertexBuffer=gl.createBuffer();
+    if (!vertexBuffer) {
+      console.log('创建缓冲区对象失败');
+      reuturn -1;
+    }
+    //将缓冲区对象绑定到目标
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    //向缓充区对象写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    
+    // 获取attribute变量的存储位置
+    var a_Position =gl.getAttribLocation(gl.program,'a_Position');
+    if(a_Position<0){
+        conosole.log('尝试获取a_Position失败');
+        return;
+    }
+    //将缓冲区对象分配给a_position对象
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    //连接a_Position变量与分配给它的缓冲区对象
+    gl.enableVertexAttribArray(a_Position);
+
+    
+    var u_Width = gl.getUniformLocation(gl.program,'u_Width');
+    var u_Height = gl.getUniformLocation(gl.program,'u_Height');
+    if(!u_Width||!u_Height){
+        console.log('获取角度信息设置失败');
+        return ;
+    }
+
+    gl.uniform1f(u_Width, gl.drawingBufferWidth);
+    gl.uniform1f(u_Height, gl.drawingBufferHeight);
+    return n;
+}
+
+
+ 
